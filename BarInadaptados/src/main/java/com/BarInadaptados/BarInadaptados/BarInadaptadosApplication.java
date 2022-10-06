f5b9b0906090bfea9ae3f67f5c38eabdbe8e5b4b
Lautaro <lautarorizzitano@gmail.com>
2022-10-06 00:53:18 -0300

    Creado el proyecto spring con el conector sql y las dependencias

---
+++ BarInadaptados/src/main/java/com/BarInadaptados/BarInadaptados/BarInadaptadosApplication.java
@@ -0,0 +1,16 @@
+package com.BarInadaptados.BarInadaptados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarInadaptadosApplication {
+
+	//Agregamos el conector SQL a applications.properties y las dependencias de SQL y la API java a pom.xml
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BarInadaptadosApplication.class, args);
+		
+	}
+
+}
---
+++ BarInadaptados/src/test/java/com/BarInadaptados/BarInadaptados/BarInadaptadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarInadaptados.BarInadaptados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarInadaptadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
